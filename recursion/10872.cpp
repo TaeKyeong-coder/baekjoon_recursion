6928e5c1a1b9ec84683cd8dba0a9bb3bf300e754
태고밍 <68893329+TaeKyeong-coder@users.noreply.github.com>
2022-12-12 16:30:13 +0900

    피보나치수열

--- recursion/10872.cpp
+++ recursion/10872.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
-int factorial(int);
+int fiv(int);
 
 int main(void) {
-	int N, result = 1;
-	cin >> N;
-	if (N != 0)
-		result = factorial(N);
-	cout << result;
+	//0, 1, 0+1=1, 1+1=2...
+	int n;
+	cin >> n;
+
+	int answer = fiv(n);
+
+	cout << answer;
 }
 
-int factorial(int n) {
-	if (n > 2)
-		n *= factorial(n - 1);
-	return n;
+int fiv(int f) {
+	if (f == 0)
+		return 0;
+	else if (f == 1) return 1;
+	else
+		return fiv(f - 1) + fiv(f - 2);
 }
